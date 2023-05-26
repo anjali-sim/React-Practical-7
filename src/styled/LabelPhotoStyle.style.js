c5b152700ffbf6954a716c8496e176447c8f57a1
anjali-sim <anjalighetia123@gmail.com>
2023-05-26 15:05:39 +0530

    :lipstick: Optimized the code in the styled file

--- src/styled/LabelPhotoStyle.style.js
+++ src/styled/LabelPhotoStyle.style.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const LabelPhotoStyle = styled.label`
 font-size: 15px;
 color: black;
+margin-left: 175px;
 `;
 
 export default LabelPhotoStyle;
--- src/styled/PhotoFileStyle.style.js
+++ src/styled/PhotoFileStyle.style.js
@@ -2,35 +2,8 @@ import styled from "styled-components";
 
 // Style for the input type file
 const PhotoFileStyle = styled.input`
-background-color: white;
-margin:bottom: 10px;
-border: none;
-margin-top: 8px;
-margin-bottom: 10px;
-text-align: left;
-width: 100%;
-// display: none;
-visibility: hidden;
-cursor: pointer;
-color: transparent;
-text-shadow: 0 0 0 #000;
-  cursor: pointer;
+  width: 100%;
+  visibility: hidden;
 `;
-
-// const PhotoFileStyle = styled.input.attrs({ type: 'file'})`
-// background-color: white;
-// margin:bottom: 10px;
-// border: none;
-// margin-top: 8px;
-// margin-bottom: 10px;
-// text-align: left;
-// width: 100%;
-// cursor: pointer;
-// display: none;
-
-// color: transparent;
-// text-shadow: 0 0 0 #000;
-//   cursor: pointer;
-// `;
 
 export default PhotoFileStyle;
