c1b99597f4ec0066d3c03d78901fda60546b63a2
anjali-sim <anjalighetia123@gmail.com>
2023-06-02 15:16:54 +0530

    :sparkles: Included the public and private route, Added a button on Not Found page to redirect the user to Home or Signup page, Improved the structure of the files

--- src/components/Label/Label.jsx
+++ src/components/Label/Label.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import LabelStyle from "@src/components/Label/LabelStyle.style";
 
-const LabelComponent = ({ htmlFor, children }) => {
+const Label = ({ htmlFor, children }) => {
   return <LabelStyle htmlFor={htmlFor}>{children}</LabelStyle>;
 };
 
-LabelComponent.propTypes = {
+Label.defaultProps = {
+  children: null,
+};
+
+Label.propTypes = {
   htmlFor: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
 
-export default LabelComponent;
+export default Label;
